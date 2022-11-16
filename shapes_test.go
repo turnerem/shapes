e9b5777518e863a0df2ee23476eb9709ef1af127
turnerem <turnerem@tcd.ie>
2022-11-16 15:10:35 +0000

    Add Circle struct and change Area and Perimeter to methods on struct

--- shapes_test.go
+++ shapes_test.go
@@ -3,26 +3,55 @@ package shapes_test
 import (
   shapes "github.com/turnerem/shapes"
   "testing"
+  "math"
 )
 
 func TestPerimeter(t *testing.T) {
-  rect := shapes.Rectangle{10.0, 10.0}
+  t.Run("rectangles perim", func(t *testing.T) {
 
-  got := shapes.Perimeter(rect)
-  want := 40.0
+    rect := shapes.Rectangle{10.0, 10.0}
 
-  if got != want {
-    t.Errorf("got %.2f but want %.2f", got, want)
-  }
+    got := rect.Perimeter()
+    want := 40.0
+
+    if got != want {
+      t.Errorf("got %.2f but want %.2f", got, want)
+    }
+  })
+
+  t.Run("circles perim", func(t *testing.T) {
+    circle := shapes.Circle{10.0}
+
+    got := circle.Perimeter()
+    want := 2 * math.Pi * 10.0
+
+    if got != want {
+      t.Errorf("got %.2f but want %.2f", got, want)
+    }
+  })
 }
 
 func TestArea(t *testing.T) {
-  rect := shapes.Rectangle{10.0, 10.0}
+    t.Run("rectangles area", func(t *testing.T) {
 
-  got := shapes.Area(rect)
-  want := 100.0
+    rect := shapes.Rectangle{10.0, 10.0}
 
-  if got != want {
-    t.Errorf("got %.2f but want %.2f", got, want)
-  }
+    got := rect.Area()
+    want := 100.0
+
+    if got != want {
+      t.Errorf("got %.2f but want %.2f", got, want)
+    }
+  })
+
+  t.Run("circles area", func(t *testing.T) {
+    circle := shapes.Circle{10}
+
+    got := circle.Area()
+    want := math.Pi * 10 * 10
+
+    if got != want {
+      t.Errorf("got %.2f but want %.2f", got, want)
+    }
+  })
 }
